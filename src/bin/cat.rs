a9ec21d785ce48c223dc49250fd203a58f47bbf4
yahsieh <yunganh@uci.edu>
2022-11-06 22:30:21 -0800

    Init and add bin/cat.rs

---
+++ src/bin/cat.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, Read, Write};
+
+fn cat(path: &str) -> io::Result<()> {
+    let file = File::open(path)?;
+    let mut buf_reader = io::BufReader::new(file);
+    let mut buffer = String::new();
+    buf_reader.read_to_string(&mut buffer)?;
+    io::stdout().write(buffer.as_bytes())?;
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    for path in &args[1..] {
+        cat(path)?;
+    }
+    Ok(())
+}
